a6a28dcc93ca6d32fe3084b08e520ad5b8053272
EthanHolleman <eholleman@luc.edu>
2019-11-16 12:12:22 -0600

    Moved non class functions to run_utils.py

---
+++ Run/run_utils.py
@@ -0,0 +1,22 @@
+import os
+
+
+def get_intact_length(con_file_path):
+    check_formating(con_file_path)
+    length = []
+    with open(con_file_path) as con:
+        length = con.readlines()
+
+    return len(length[1])
+
+
+def make_clstr(fasta, clstr_dir):
+    clstr_file_name = make_clstr_name(fasta, clstr_dir)
+    print('Clustering', fasta)
+    run_cd_hit(clstr_file_name, fasta)
+    return clstr_file_name + '.clstr'
+
+
+def make_clstr_name(path, clstr_dir):
+    base = os.path.basename(path).split('.')[0]
+    return os.path.join(clstr_dir, base)
