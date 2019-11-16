5fc16fd35082873e4c1f53addedab5295fb92d51
EthanHolleman <eholleman@luc.edu>
2019-11-16 12:14:03 -0600

    Removed par package and deperciated modules

--- stats.py
+++
@@ -1,9 +0,0 @@
-def number_types(element_list):
-    s, i = 0, 0
-    for el in element_list:
-        if el.type == 'S':
-            s+=1
-        elif el.type == 'I':
-            i+=1
-
-    return tuple([i, s])
